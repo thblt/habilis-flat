d8152434ebba07050a1d5f0505bcc88cefe5dbdf
Thibault Polge <thibault@thb.lt>
2023-11-13 14:08:39 +0100

    Align keymap

--- fw/keymaps/default/keymap.c
+++ fw/keymaps/default/keymap.c
@@ -5,15 +5,22 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
+     * ┌───┬───┬───┬───┬───┬───┐ ┌───┬───┬───┬───┬───┬───┐
+     * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ │ 6 │ 7 │ 8 │ 9 │ 0 │ - │
+     * ├───┼───┼───┼───┼───┼───┤ ├───┼───┼───┼───┼───┼───┤
+     * │ } │ Q │ W │ E │ R │ T │ │ Y │ U │ I │ O │ P │ { │
+     * ├───┼───┼───┼───┼───┼───┤ ├───┼───┼───┼───┼───┼───┤
+     * │   │ A │ S │ D │ F │ G │ │ H │ J │ K │ L │ : │ " │
+     * ├───┼───┼───┼───┼───┼───┤ ├───┼───┼───┼───┼───┼───┤
+     * │   │ Z │ X │ C │ V │ B │ │ N │ M │ , │ . │ / │   │
+     * ├───┼───┼───┼───┼───┼───┘ └───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   │         │   │   │   │   │   │
+     * └───┴───┴───┴───┴───┘         └───┴───┴───┴───┴───┘
+     * ┌───────┬───────┬───────┐ ┌───────┬───────┬───────┐
+     * │ Shift │       │  Del  │ │ Super │       │ BkSpc │
+     * ├───────┼───────┼───────┤ ├───────┼───────┼───────┤
+     * │       │ Space │ LCtrl │ │  Alt  │ Enter │ AltGr │
+     * └───────┴───────┴───────┘ └───────┴───────┴───────┘
      */
     [0] = LAYOUT(
                  // Left hand
@@ -21,10 +28,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_RBRC , KC_Q , KC_W , KC_E,  KC_R,  KC_T,
                  KC_NO   , KC_A , KC_S , KC_D,  KC_F,  KC_G,
                  KC_NUBS , KC_Z , KC_X , KC_C,  KC_V,  KC_B,
-                 KC_NO   , KC_NO, KC_NO, KC_NO, KC_NO,
+                 KC_NO   , KC_NO, KC_NO, KC_NO, KC_NO, /* */
                  // Left thumb cluster
-                 KC_LSFT, KC_NO, KC_DEL,
-                 KC_NO, KC_SPC, KC_LCTL,
+                 KC_LSFT, KC_NO,  KC_DEL,
+                 KC_NO,   KC_SPC, KC_LCTL,
 
                  // Right hand
                  KC_6  , KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS,
@@ -33,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_N  , KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_NO,
                  /*   */ KC_NO, KC_NO,   KC_NO,  KC_NO,   KC_NO,
                  // Right thumb cluster
-                 KC_LGUI, KC_NO, KC_BSPC,
+                 KC_LGUI, KC_NO,  KC_BSPC,
                  KC_LALT, KC_ENT, KC_RALT
                  )
 };
